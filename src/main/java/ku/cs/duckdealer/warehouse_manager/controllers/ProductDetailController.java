8590b297468ce10b70fe158381c6c57f373c6684
Aminomous <yoyothunder@gmail.com>
2017-09-26 16:21:53 +0700

    Update Owner can add (waiting database) Adjust window field appearance in product detail
    Time taken
    24/9/2017
    15.15 - 16.00

--- src/main/java/ku/cs/duckdealer/warehouse_manager/controllers/ProductDetailController.java
+++ src/main/java/ku/cs/duckdealer/warehouse_manager/controllers/ProductDetailController.java
@@ -1,20 +1,13 @@
 package ku.cs.duckdealer.warehouse_manager.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 import ku.cs.duckdealer.warehouse_manager.models.Product;
-
-import java.io.IOException;
+import ku.cs.duckdealer.warehouse_manager.models.StockedProduct;
 
 public class ProductDetailController {
 
@@ -23,64 +16,73 @@ public class ProductDetailController {
     @FXML
     private Label remainAmountLabel, idLabel, incSpaceLabel, decSpaceLabel;
     @FXML
-    private Button btnIncrease, btnDecrease, btnEdit;
+    private Button btnIncrease, btnDecrease, btnEdit, btnOk, btnCancel;
     @FXML
     private FlowPane amountArea;
 
-    private Product product;
+    private StockedProduct stockedProduct;
     private MainController mainCtrl;
     private boolean isEditing = false;
     private BorderPane mainPane;
-    private Button source;
+
 
     @FXML
     private void initialize() {
+        this.nameField.clear();
+        this.priceField.clear();
+
+        this.nameField.setDisable(true);
+        this.priceField.setDisable(true);
+
         this.amountArea.getChildren().remove(this.btnDecrease);
         this.amountArea.getChildren().remove(this.decSpaceLabel);
         this.amountArea.getChildren().remove(this.remainAmountLabel);
         this.amountArea.getChildren().remove(this.incSpaceLabel);
         this.amountArea.getChildren().remove(this.btnIncrease);
+        this.btnOk.setVisible(false);
+        this.btnCancel.setVisible(false);
 
         this.amountArea.getChildren().add(this.remainAmountLabel);
     }
 
-    public void setup(Product p) {
-        this.product = p;
-        this.idLabel.setText(this.product.getID());
+    public void setup(StockedProduct p) {
+        this.stockedProduct = p;
+        this.idLabel.setText(this.stockedProduct.getProduct().getID());
         this.nameField.setEditable(false);
-        this.nameField.setText(this.product.getName());
+        this.nameField.setText(this.stockedProduct.getProduct().getName());
         this.priceField.setEditable(false);
-        this.priceField.setText(this.product.getPrice()+"");
-        this.remainAmountLabel.setText(this.product.getQuantity()+"");
+        this.priceField.setText(this.stockedProduct.getProduct().getPrice()+"");
+        this.remainAmountLabel.setText(this.stockedProduct.getQuantity()+"");
 
-        this.amountArea.getChildren().remove(this.btnDecrease);
-        this.amountArea.getChildren().remove(this.decSpaceLabel);
-        this.amountArea.getChildren().remove(this.remainAmountLabel);
-        this.amountArea.getChildren().remove(this.incSpaceLabel);
-        this.amountArea.getChildren().remove(this.btnIncrease);
+        initialize();
+    }
 
-        this.amountArea.getChildren().add(this.remainAmountLabel);
+    public void updateAmount(){
 
     }
 
-    public void updateAmount(ActionEvent event){
-        String text = ((Button)event.getSource()).getText();
-        if(text.equals("+")){
-            Stage stage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/increaseProductPopUp.fxml"));
-            stage.initModality(Modality.APPLICATION_MODAL);
-            try {
-                stage.setScene(new Scene((Parent) loader.load()));
-                AmountController amountController = loader.getController();
-                stage.showAndWait();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            product.setQuantity(mainCtrl.getAmountController().getIncreaseAmount());
+    public void toggleCreateMode(){
+        this.nameField.setDisable(false);
+        this.priceField.setDisable(false);
 
-        }else if (text.equals("-")){
+        this.nameField.setEditable(true);
+        this.priceField.setEditable(true);
+        this.btnEdit.setVisible(false);
+        this.btnOk.setVisible(true);
+        this.btnCancel.setVisible(true);
 
+    }
+    public void createProduct(){
+        if (AuthenticationService.NOT_LOGGED_IN){
+            mainCtrl.login();
         }
+        if (!AuthenticationService.NOT_LOGGED_IN){
+            stockedProduct = new StockedProduct(nameField.getText(), Integer.parseInt(priceField.getText()));
+            initialize();
+        }
+    }
+
+    public void cancel(){
     }
 
     public void toggleEditMode() {
@@ -103,8 +105,6 @@ public class ProductDetailController {
                 this.nameField.setEditable(true);
                 this.priceField.setEditable(true);
             }
-
-
         } else {
 
             if (AuthenticationService.LOGGED_IN_AS_OWNER) {
