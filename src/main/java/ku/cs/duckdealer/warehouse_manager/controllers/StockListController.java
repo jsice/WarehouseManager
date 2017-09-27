2ccbf76e813624c12156cef0a82ea803568395bf
jsice <w14chin@gmail.com>
2017-09-27 11:33:43 +0700

    Products which is displayed can now selected and show its detail.

--- src/main/java/ku/cs/duckdealer/warehouse_manager/controllers/StockListController.java
+++ src/main/java/ku/cs/duckdealer/warehouse_manager/controllers/StockListController.java
@@ -1,14 +1,15 @@
 package ku.cs.duckdealer.warehouse_manager.controllers;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.RowConstraints;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import ku.cs.duckdealer.warehouse_manager.models.Product;
 import ku.cs.duckdealer.warehouse_manager.models.StockedProduct;
 
@@ -21,6 +22,12 @@ public class StockListController {
     private ArrayList<Label> labels;
     private MainController mainCtrl;
 
+    private String searchText;
+
+    private Label selectedID, selectedName, selectedPrice, selectedAmount;
+
+    private BackgroundFill selectedBackgroundFill;
+
     @FXML
     private GridPane innerTableGrid;
     @FXML
@@ -32,6 +39,8 @@ public class StockListController {
     private void initialize() {
         this.stockedProducts = new ArrayList<StockedProduct>();
         labels = new ArrayList<Label>();
+        searchText = "";
+        selectedBackgroundFill = new BackgroundFill(Color.CORAL, CornerRadii.EMPTY, Insets.EMPTY);
     }
 
     public void createNewProduct(){
@@ -45,43 +54,81 @@ public class StockListController {
 
     }
 
-    @FXML
-    private void showProducts() {
-        if (searchTextfield.getText() == null) {
-            stockedProducts = this.mainCtrl.getStock().getAllProducts();
-        } else {
-            String text = searchTextfield.getText();
-            this.stockedProducts.removeAll(this.stockedProducts);
-            for (StockedProduct p: this.mainCtrl.getStock().getAllProducts()) {
-                if (p.getProduct().getID().contains(text)) {
-                    stockedProducts.add(p);
-                }
-            }
-        }
-
+    private void clearGrid() {
+        selectedID = null;
+        selectedName = null;
+        selectedPrice = null;
+        selectedAmount = null;
         innerTableGrid.getChildren().clear();
         innerTableGrid.setPrefHeight(400);
         this.innerTableGrid.setGridLinesVisible(false);
         this.innerTableGrid.setGridLinesVisible(true);
 
+    }
+
+    private void filterStockedProducts() {
+        this.stockedProducts.removeAll(this.stockedProducts);
+        if (searchText == null) {
+            stockedProducts.addAll(this.mainCtrl.getStock().getAllProducts());
+        } else {
+            for (StockedProduct p: this.mainCtrl.getStock().getAllProducts()) {
+                if (p.getProduct().getID().contains(searchText)) {
+                    stockedProducts.add(p);
+                }
+            }
+        }
+    }
+
+    private void displayFilteredProducts() {
+        clearGrid();
         int row = 0;
-        for (StockedProduct p: this.stockedProducts) {
+        for (final StockedProduct p: this.stockedProducts) {
             if (row >= 10) {
                 this.innerTableGrid.setPrefHeight(this.innerTableGrid.getPrefHeight() + 40);
                 this.innerTableGrid.addRow(row);
             }
-            Label id = new Label(p.getProduct().getID()+"");
-            Label name = new Label(p.getProduct().getName());
-            Label price = new Label(p.getProduct().getPrice()+"");
-            Label amount = new Label(p.getQuantity()+"");
+            final Label id = new Label(p.getProduct().getID()+"");
+            final Label name = new Label(p.getProduct().getName());
+            final Label price = new Label(p.getProduct().getPrice()+"");
+            final Label amount = new Label(p.getQuantity()+"");
             id.setPrefHeight(38);
-            id.setPrefWidth(99);
+            id.setPrefWidth(122);
             name.setPrefHeight(38);
-            name.setPrefWidth(100);
+            name.setPrefWidth(123);
             price.setPrefHeight(38);
-            price.setPrefWidth(100);
+            price.setPrefWidth(123);
             amount.setPrefHeight(38);
-            amount.setPrefWidth(99);
+            amount.setPrefWidth(122);
+
+            EventHandler<MouseEvent> productLabelEventHandler = new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent event) {
+                    mainCtrl.showProductDetail(p);
+                    if (selectedID != null) {
+                        selectedID.setBackground(null);
+                        selectedName.setBackground(null);
+                        selectedPrice.setBackground(null);
+                        selectedAmount.setBackground(null);
+//                        selectedID.getBackground().getFills().remove(selectedBackgroundFill);
+//                        selectedName.getBackground().getFills().remove(selectedBackgroundFill);
+//                        selectedPrice.getBackground().getFills().remove(selectedBackgroundFill);
+//                        selectedAmount.getBackground().getFills().remove(selectedBackgroundFill);
+                    }
+                    selectedID = id;
+                    selectedName = name;
+                    selectedPrice = price;
+                    selectedAmount = amount;
+                    selectedID.setBackground(new Background(selectedBackgroundFill));
+                    selectedName.setBackground(new Background(selectedBackgroundFill));
+                    selectedPrice.setBackground(new Background(selectedBackgroundFill));
+                    selectedAmount.setBackground(new Background(selectedBackgroundFill));
+                }
+            };
+
+            id.setOnMouseClicked(productLabelEventHandler);
+            name.setOnMouseClicked(productLabelEventHandler);
+            price.setOnMouseClicked(productLabelEventHandler);
+            amount.setOnMouseClicked(productLabelEventHandler);
+
             this.labels.add(id);
             this.labels.add(name);
             this.labels.add(price);
@@ -91,9 +138,14 @@ public class StockListController {
             this.innerTableGrid.add(price, 2, row);
             this.innerTableGrid.add(amount, 3, row);
             row++;
-
-
         }
+    }
+
+    @FXML
+    private void showProducts() {
+        searchText = searchTextfield.getText();
+        this.filterStockedProducts();
+        this.displayFilteredProducts();
 
     }
 
