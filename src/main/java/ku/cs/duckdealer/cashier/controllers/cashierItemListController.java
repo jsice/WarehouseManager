6cc0581c8ee8332b5fc729c5cf2b58b6bae4b8b2
moiammo <yarnadhis.k@ku.th>
2017-09-28 19:14:52 +0700

    not done yet

--- src/main/java/ku/cs/duckdealer/cashier/controllers/cashierItemListController.java
+++ src/main/java/ku/cs/duckdealer/cashier/controllers/cashierItemListController.java
@@ -1,22 +1,35 @@
 package ku.cs.duckdealer.cashier.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import ku.cs.duckdealer.models.Stock;
+import ku.cs.duckdealer.models.StockedProduct;
 //import ku.cs.duckdealer.cashier.models.StockedProduct;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class cashierItemListController {
 
     private Pane mainPane;
-//    private ArrayList<StockedProduct> stockedProducts;
     private ArrayList<Label> labels;
     private MainController mainCtrl;
 
     @FXML
     private TextField searchTextfield;
+    @FXML
+    private ComboBox<String> filterComboBox;
+    @FXML
+    private GridPane innerTableGrid ;
+
+    private ArrayList<StockedProduct> stockedProducts;
+    private ArrayList<ArrayList> tempArray ;
+    private Stock stock ;
 
 
     public Pane getMainPane() {
@@ -31,35 +44,28 @@ public class cashierItemListController {
         this.mainCtrl = mainCtrl;
     }
 
+
     @FXML
-    private  void showProducts(){}
-//    @FXML
-//    private void showProducts() {
-//        String text = searchTextfield.getText();
-//        this.stockedProducts.removeAll(this.stockedProducts);
-//        for (StockedProduct p: this.mainCtrl.getStock().getAllProducts()) {
-//            if (p.getProduct().getID().contains(text)) {
-//                stockedProducts.add(p);
-//            }
-//        }
-//        int row = 0;
-//        for (StockedProduct p: this.stockedProducts) {
-//            Label id = new Label(p.getProduct().getID()+"");
-//            Label name = new Label(p.getProduct().getName());
-//            Label price = new Label(p.getProduct().getPrice()+"");
-//            Label amount = new Label(p.getQuantity()+"");
-//            this.labels.add(id);
-//            this.labels.add(name);
-//            this.labels.add(price);
-//            this.labels.add(amount);
-//            this.innerTableGrid.add(id, 0, row);
-//            this.innerTableGrid.add(name, 1, row);
-//            this.innerTableGrid.add(price, 2, row);
-//            this.innerTableGrid.add(amount, 3, row);
-//            row++;
-//        }
-//
-//    }
+    private void initialize() {
+
+        filterComboBox.getItems().addAll("ID", "Name");
+        stockedProducts = stock.getAllProducts();
+        stock.newProduct(new StockedProduct("Apple",20.0));
+
+
+
+    }
+
+    @FXML
+    private void showProducts() {}
+
+    private void showAllStockedProduct(){
+
+
+    }
+
+
+
 
 
 }
