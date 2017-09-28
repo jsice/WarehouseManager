3b4724f88a9dcfec4df2e9c8267f526a27118542
moiammo <yarnadhis.k@ku.th>
2017-09-28 15:42:20 +0700

    cashier controllers and fxml

---
+++ src/main/java/ku/cs/duckdealer/cashier/controllers/MainPaneController.java
@@ -0,0 +1,36 @@
+package ku.cs.duckdealer.cashier.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+
+public class MainPaneController {
+
+    private MainController mainCtrl;
+    private FlowPane mainPane;
+
+    @FXML
+    private GridPane leftPane, rightPane;
+
+
+    public FlowPane getMainPane() {
+        return mainPane;
+    }
+
+    public void setMainPane(FlowPane mainPane) {
+        this.mainPane = mainPane;
+    }
+
+    public GridPane getLeftPane() {
+        return leftPane;
+    }
+
+    public GridPane getRightPane() {
+        return rightPane;
+    }
+
+
+    public void setMainCtrl(MainController mainCtrl) {
+        this.mainCtrl = mainCtrl;
+    }
+}
