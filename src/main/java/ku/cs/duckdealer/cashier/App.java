dbfd369c0b857be247a3bdd106c5278e5d760fd7
moiammo <yarnadhis.k@ku.th>
2017-09-28 15:39:03 +0700

    cashier controllers and fxml

--- src/main/java/ku/cs/duckdealer/cashier/App.java
+++ src/main/java/ku/cs/duckdealer/cashier/App.java
@@ -2,11 +2,16 @@ package ku.cs.duckdealer.cashier;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import ku.cs.duckdealer.cashier.controllers.MainController;
 
-public class App extends Application {
-    public void start(Stage primaryStage) throws Exception {
-
+public class App  extends  Application
+{
+    public static void main( String[] args ) {
+        launch();
     }
 
-    public static void main(String[] args) { launch(); }
+    public void start(Stage primaryStage) throws Exception {
+        MainController main = new MainController(primaryStage);
+        main.start();
+    }
 }
