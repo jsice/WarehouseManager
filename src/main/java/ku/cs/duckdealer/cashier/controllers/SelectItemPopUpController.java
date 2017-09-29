09fb9aed89c8c048aba8aa09d5b869c020634afa
moiammo <yarnadhis.k@ku.th>
2017-09-29 12:48:53 +0700

    cashier big update

---
+++ src/main/java/ku/cs/duckdealer/cashier/controllers/SelectItemPopUpController.java
@@ -0,0 +1,47 @@
+package ku.cs.duckdealer.cashier.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import ku.cs.duckdealer.models.StockedProduct;
+
+public class SelectItemPopUpController {
+
+
+    private int amount ;
+
+    @FXML
+    private Label prodIdLabel, prodNameLabel , maxAmountLabel ;
+    @FXML
+    private TextField amountField ;
+
+    @FXML
+    private void handleBtnOk() {
+        amount = Integer.parseInt(amountField.getText()) ;
+        if (amount > Integer.parseInt(maxAmountLabel.getText())){
+            amount = Integer.parseInt(maxAmountLabel.getText()) ;
+        }
+        amountField.getScene().getWindow().hide();
+    }
+
+    @FXML
+    private void handleBtnCancel(){
+        amountField.getScene().getWindow().hide();
+    }
+
+    public int getAmount (){
+        return amount ;
+    }
+
+    public void setAllLabelText(StockedProduct selectedProduct) {
+        prodIdLabel.setText(selectedProduct.getProduct().getID());
+        prodNameLabel.setText(selectedProduct.getProduct().getName());
+        maxAmountLabel.setText(selectedProduct.getQuantity()+"" );
+        amountField.setPromptText("1"); // 1 by default
+    }
+
+
+
+
+
+}
